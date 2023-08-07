5c818a5d60db36cdbcd48b85d51c10d1679b89bf
TechnoStudy <ismettechno@gmail.com>
2023-08-07 18:10:57 +0100

    Gun01

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,12 @@
+package Gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("merhaba 1");
+
+        System.out.println("merhaba 2");
+
+
+    }
+}
